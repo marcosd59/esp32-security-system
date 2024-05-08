c37ceb9733dfbfec06028d02d5636b0462ed1d83
Marcos Damián Pool Canul <damian.marcospool@gmail.com>
2024-05-07 20:18:13 -0500

    feat: Add responsive app bar component to the main app page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ResponsiveAppBar from "./components/AppBar";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
