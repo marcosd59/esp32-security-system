4d80942128586ab704443313ac103b958d8ac34c
Marcos Damián Pool Canul <damian.marcospool@gmail.com>
2024-05-11 07:19:00 -0500

    chore: Update images in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -45,7 +45,7 @@ export default function MediaCover() {
         >
           <CardCover>
             <img
-              src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+              src="https://www.espressif.com/sites/default/files/ESP32-CAM-Take-Photo-Display-Web-Server.jpg"
               srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
               loading="lazy"
               alt=""
@@ -58,39 +58,7 @@ export default function MediaCover() {
               textColor="#fff"
               mt={{ xs: 12, sm: 18 }}
             >
-              Image
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card
-          component="li"
-          sx={{
-            minWidth: 300,
-            maxWidth: "65%",
-            flexGrow: 1,
-          }}
-        >
-          <CardCover>
-            <video
-              autoPlay
-              loop
-              muted
-              poster="https://assets.codepen.io/6093409/river.jpg"
-            >
-              <source
-                src="https://assets.codepen.io/6093409/river.mp4"
-                type="video/mp4"
-              />
-            </video>
-          </CardCover>
-          <CardContent>
-            <Typography
-              level="body-lg"
-              fontWeight="lg"
-              textColor="#fff"
-              mt={{ xs: 12, sm: 18 }}
-            >
-              Video
+              ESP32 Cam
             </Typography>
           </CardContent>
         </Card>
@@ -104,7 +72,7 @@ export default function MediaCover() {
         >
           <CardCover>
             <img
-              src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+              src="https://images.datacamp.com/image/upload/f_auto,q_auto:best/v1603718736/Why_Your_Company_Needs_Python_for_Business_Analytics_xzzles.png"
               srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
               loading="lazy"
               alt=""
@@ -117,7 +85,34 @@ export default function MediaCover() {
               textColor="#fff"
               mt={{ xs: 12, sm: 18 }}
             >
-              Image
+              Python
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card
+          component="li"
+          sx={{
+            minWidth: 300,
+            maxWidth: "45%",
+            flexGrow: 1,
+          }}
+        >
+          <CardCover>
+            <img
+              src="https://media.licdn.com/dms/image/C4E12AQFdVr18zUa17Q/article-cover_image-shrink_720_1280/0/1624637761724?e=2147483647&v=beta&t=txL8oZw9vpsEAev7vCuRj8GQpDmbmG9FMUnRDtRzXaY"
+              srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
+              loading="lazy"
+              alt=""
+            />
+          </CardCover>
+          <CardContent>
+            <Typography
+              level="body-lg"
+              fontWeight="lg"
+              textColor="#fff"
+              mt={{ xs: 12, sm: 18 }}
+            >
+              React
             </Typography>
           </CardContent>
         </Card>
