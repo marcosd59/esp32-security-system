2e85da36fe47d664d7fe1bf50813297ef4452b70
Marcos Damián Pool Canul <damian.marcospool@gmail.com>
2024-05-10 21:50:06 -0500

    feat: Add routing and components for video streaming and map display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResponsiveAppBar from "./components/AppBar";
+import VideoStream from "./components/VideoStream";
+import MapContainer from "./components/MapContainer";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <ResponsiveAppBar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ResponsiveAppBar />
+
+        <Routes>
+          <Route path="/" element={<Card />} />
+          <Route path="/camara" element={<VideoStream />} />
+          <Route path="/ubicacion" element={<MapContainer />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
