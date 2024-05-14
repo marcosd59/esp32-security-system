0e8132831fada1e24ec39a26a6c2406641ceb6a8
Marcos Damián Pool Canul <damian.marcospool@gmail.com>
2024-05-14 11:41:42 -0500

    chore: Update video streaming URL to use port 81

--- src/server/index.py
+++ src/server/index.py
@@ -1,16 +1,20 @@
-from flask import Flask, Response
-import face_recognition
+from flask import Flask, Response, jsonify
 import cv2
+import face_recognition
 import pickle
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-with open('D:\CARPETA_DE_EVIDENCIAS_SEMESTRE_8\esp32-security-system\src\server\encodings.pickle', 'rb') as f:
-    data = pickle.load(f)
+with open('D:\\CARPETA_DE_EVIDENCIAS_SEMESTRE_8\\esp32-security-system\\src\\server\\encodings.pickle', 'rb') as f:
+    known_data = pickle.load(f)
+
+unknown_detected = False
 
 
 def generate_frames():
-
+    global unknown_detected
     video_url = "http://192.168.1.252:81/stream"
     video_capture = cv2.VideoCapture(video_url)
 
@@ -18,40 +22,44 @@ def generate_frames():
         success, frame = video_capture.read()
         if not success:
             break
-        else:
-            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            boxes = face_recognition.face_locations(rgb, model='hog')
-            encodings = face_recognition.face_encodings(rgb, boxes)
 
-            names = []
-            for encoding in encodings:
-                matches = face_recognition.compare_faces(
-                    data['encodings'], encoding)
-                name = 'Unknown'
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        boxes = face_recognition.face_locations(rgb, model='hog')
+        encodings = face_recognition.face_encodings(rgb, boxes)
 
-                if True in matches:
-                    matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-                    counts = {}
+        names = []
+        unknown_detected = False
 
-                    for i in matchedIdxs:
-                        name = data['names'][i]
-                        counts[name] = counts.get(name, 0) + 1
+        for encoding in encodings:
+            matches = face_recognition.compare_faces(
+                known_data['encodings'], encoding)
+            name = 'Unknown'
 
-                    name = max(counts, key=counts.get)
+            if True in matches:
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
 
-                names.append(name)
+                for i in matchedIdxs:
+                    name = known_data['names'][i]
+                    counts[name] = counts.get(name, 0) + 1
 
-            for ((top, right, bottom, left), name) in zip(boxes, names):
-                cv2.rectangle(frame, (left, top),
-                              (right, bottom), (0, 255, 0), 2)
-                y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(frame, name, (left, y),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+                name = max(counts, key=counts.get)
 
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            if name == 'Unknown':
+                unknown_detected = True
+
+            names.append(name)
+
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(frame, name, (left, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video')
@@ -60,5 +68,11 @@ def video():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@app.route('/check_for_unknowns')
+def check_for_unknowns():
+    global unknown_detected
+    return jsonify({'unknownDetected': unknown_detected})
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
